baa7519f988e58395a577606ce37944825f74eb1
pablovicz <pablowoina2205@gmail.com>
2020-09-07 15:01:22 -0300

    Commit: initial commit

---
+++ Upload-Download-Example/src/main/java/br/com/pablovicz/UploadDownloadExample/UploadDownloadExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.pablovicz.UploadDownloadExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadDownloadExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadDownloadExampleApplication.class, args);
+	}
+
+}
---
+++ Upload-Download-Example/src/test/java/br/com/pablovicz/UploadDownloadExample/UploadDownloadExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pablovicz.UploadDownloadExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadDownloadExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
