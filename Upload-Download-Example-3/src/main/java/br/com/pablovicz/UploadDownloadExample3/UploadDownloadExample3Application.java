de7ea3dca82c1c6eaa46a5e9ad403d2ea651129d
pablovicz <pablowoina2205@gmail.com>
2020-09-08 16:44:17 -0300

    Commit: 1.5 version

---
+++ Upload-Download-Example-3/src/main/java/br/com/pablovicz/UploadDownloadExample3/UploadDownloadExample3Application.java
@@ -0,0 +1,12 @@
+package br.com.pablovicz.UploadDownloadExample3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadDownloadExample3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadDownloadExample3Application.class, args);
+	}
+}
---
+++ Upload-Download-Example-3/src/test/java/br/com/pablovicz/UploadDownloadExample3/UploadDownloadExample3ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pablovicz.UploadDownloadExample3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadDownloadExample3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
